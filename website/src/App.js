8bd7f7d6605bbefaad8cc24d9e6328bef95c045b
Max Verser Engelhard <maxvengelhard@gmail.com>
2024-09-12 13:59:58 -0500

    commenting out stripe stuff for now

--- website/src/App.js
+++ website/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 import StyledLogin from './components/StyledLogin';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+
+
+const updatedAwsConfig = {
+  "aws_project_region": "us-east-1",
+  "aws_cognito_identity_pool_id": process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+  // "aws_cloud_logic_custom": [
+  //   {
+  //       "name": "stripeAPI",
+  //       "endpoint": process.env.REACT_APP_STRIPE_END_POINT,
+  //       "region": "us-east-1"
+  //   }
+  // ]
+  // oauth: {
+  //   ...awsConfig.oauth,
+  //   redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+  //   redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  // },
+}
+
+Amplify.configure(updatedAwsConfig);
 
 function App() {
   return (
