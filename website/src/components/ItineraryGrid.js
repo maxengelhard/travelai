197e658f3536b0001fef1820c4f2e00e4fc4c19c
Max Verser Engelhard <maxvengelhard@gmail.com>
2024-09-18 14:27:18 -0500

    giving a download

--- website/src/components/ItineraryGrid.js
+++ website/src/components/ItineraryGrid.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { usePDF } from 'react-to-pdf';
 
 const ItineraryGrid = ({ itinerary }) => {
+    const { toPDF, targetRef } = usePDF({filename: 'my-itinerary.pdf'});
   if (!itinerary) return <div>No itinerary available</div>;
 
   const parseItinerary = (rawItinerary) => {
@@ -20,6 +22,17 @@ const ItineraryGrid = ({ itinerary }) => {
   const parsedItinerary = parseItinerary(itinerary);
 
   return (
+    <div className="relative">
+      <button
+        onClick={() => toPDF()}
+        className="absolute top-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+        </svg>
+        Download PDF
+      </button>
+    <div ref={targetRef}>
     <div className="space-y-8">
       {parsedItinerary.map((day, index) => (
         <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden">
@@ -34,6 +47,8 @@ const ItineraryGrid = ({ itinerary }) => {
           </div>
         </div>
       ))}
+    </div>
+    </div>
     </div>
   );
 };
